38397bb57297fe73b5cfa507ead413135ca9337e
LucasPrevedello <lukapreve@gmail.com>
2025-06-30 20:45:50 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const botao = document.getElementById("botaoMensagem");
-  const mensagem = document.getElementById("mensagem");
-
-  botao.addEventListener("click", function () {
-    mensagem.textContent = "“Se você fizer o que sempre fez, vai conseguir o que sempre conseguiu - Tony Robbins”";
-  });
-});
